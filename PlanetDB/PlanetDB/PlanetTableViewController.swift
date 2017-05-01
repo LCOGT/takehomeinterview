075348ba3c46c4eff4d2b1696d11f9ef6aa7e458
Antonio Medrano <tonedabass@gmail.com>
2017-04-30 22:12:03 -0700

    Implemented deleting planets from the table view

--- PlanetDB/PlanetDB/PlanetTableViewController.swift
+++ PlanetDB/PlanetDB/PlanetTableViewController.swift
@@ -62,25 +62,23 @@ class PlanetTableViewController: UITableViewController {
         return cell
     }
 
-    /*
+
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            planets.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
