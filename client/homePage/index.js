8eff99544ba5719a6e81005fe8978d1eb08ddafc
Balal Zuhair <balalzuhair@balals-mbp.gateway.pace.com>
2018-01-23 00:20:08 -0800

    sends post request to "/"

--- client/homePage/index.js
+++ client/homePage/index.js
@@ -4,10 +4,10 @@ const getPlanets = () => {
       if ($('td').text().indexOf(planet.ordinality) === -1) {
         const newPlanet = $(
           `<tr key=${planet.ordinality}>
-          <td style="padding:0 20px 0 20px;">${planet.ordinality}</td>
-          <td style="padding:0 20px 0 20px;">${planet.name}</td>
-          <td style="padding:0 20px 0 20px;">${planet.size}</td>
-          <td style="padding:0 20px 0 20px;">${planet.distance}</td>
+          <td class="ordinality" style="padding:0 20px 0 20px;">${planet.ordinality}</td>
+          <td class="name" style="padding:0 20px 0 20px;">${planet.name}</td>
+          <td class="size" style="padding:0 20px 0 20px;">${planet.size}</td>
+          <td class="distance" style="padding:0 20px 0 20px;">${planet.distance}</td>
           </tr>`,
         );
         $('#planet-list').append(newPlanet);
@@ -19,14 +19,31 @@ const getPlanets = () => {
 const addPlanet = () => {
   $('#planetForm').submit((e) => {
     e.preventDefault();
-    const name = $('#name').val();
-    const size = $('#size').val();
-    const distance = $('#distance').val();
-    const ordinality = $('#ordinality').val();
-    const description = $('#description').val();
-    console.log(name, size, distance, ordinality, description)
+    const data = {
+      name: $('#name').val(),
+      size: $('#size').val(),
+      distance: $('#distance').val(),
+      ordinality: $('#ordinality').val(),
+      description: $('#description').val(),
+    };
+
+    if ($('.name').text().toLowerCase() === data.name.toLowerCase() || $('.ordinality').text() === data.ordinality) {
+      alert('Planet Name and Ordinality needs to be unique')
+    }
+    if (!parseInt(data.size, 10) || !parseInt(data.distance, 10) || !parseInt(data.ordinality, 10)) {
+      alert('Planet Size, Distance and Ordinality needs to be an Integer');
+    } else {
+      $.ajax({
+        type: 'POST',
+        url: '/',
+        data,
+        success: (planet) => {
+          console.log(planet, 'has been posted');
+        },
+      });
+      location.reload();
+    }
   });
-  getPlanets();
 };
 
 $(document).ready(() => {
