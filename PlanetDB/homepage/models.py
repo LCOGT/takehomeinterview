00c846338f7a12142035012b4420ad5ab87fbeb6
Matt Daily <matt.daily85@gmail.com>
2018-04-17 22:33:13 -0700

    Comment models file

--- PlanetDB/homepage/models.py
+++ PlanetDB/homepage/models.py
@@ -10,5 +10,6 @@ class Planet(models.Model):
     distance = models.FloatField("Enter distance from Sun in AU")
     description = models.CharField(max_length=500, help_text="Enter description of planet (500 char. max)")
 
+    #Order by ordinality
     class Meta:
         ordering = ["ordinality"]
