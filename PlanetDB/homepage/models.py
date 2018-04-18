c4f30ba3da0a4af94eafd86c013d2b9792ca821a
Matt Daily <matt.daily85@gmail.com>
2018-04-17 22:27:41 -0700

    Add Planet model

--- PlanetDB/homepage/models.py
+++ PlanetDB/homepage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Planet(models.Model):
+    #Planet model for PlanetDB application
+
+    #Fields
+    ordinality = models.IntegerField(primary_key=True, help_text="Enter ordinality of planet")
+    name = models.CharField(max_length=20, help_text="Enter planet name")
+    size = models.FloatField("Enter size of planet in Earth Masses")
+    distance = models.FloatField("Enter distance from Sun in AU")
+    description = models.CharField(max_length=500, help_text="Enter description of planet (500 char. max)")
+
+    class Meta:
+        ordering = ["ordinality"]
