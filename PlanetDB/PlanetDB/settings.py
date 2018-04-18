601c2acea98f6f7df149c17d04798d50fe7196f7
Matt Daily <matt.daily85@gmail.com>
2018-04-17 21:51:16 -0700

    Add homepage and change timezone

--- PlanetDB/PlanetDB/settings.py
+++ PlanetDB/PlanetDB/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage.apps.HomepageConfig'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
