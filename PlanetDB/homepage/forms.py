174b8ff6a8be28ff61b47ee9438fa4e2b4c03a6b
Matt Daily <matt.daily85@gmail.com>
2018-04-23 13:13:04 -0700

    Make forms functional, add some error checks to user input

--- PlanetDB/homepage/forms.py
+++ PlanetDB/homepage/forms.py
@@ -1,9 +1,15 @@
+from django.forms import ModelForm
+from .models import Planet
 from django import forms
 
-class AddPlanetForm(forms.Form):
+class AddPlanetForm(ModelForm):
      #Fields for users to add planets
-     ordinality = forms.IntegerField(help_text="Enter ordinality of planet")
-     name = forms.CharField(max_length=20, help_text="Enter planet name")
-     size = forms.FloatField(help_text = "Enter size of planet in Earth Masses")
-     distance = forms.FloatField(help_text = "Enter distance from Sun in AU")
-     description = forms.CharField(max_length=500, help_text= "Enter description of planet (500 char. max)")
+     ordinality = forms.IntegerField(help_text="Enter ordinality of planet",
+                                      min_value = 1,
+                                      max_value = 8)
+     class Meta:
+        model = Planet
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'rows':4, 'cols': 50})
+        }
--- PlanetDB/homepage/views.py
+++ PlanetDB/homepage/views.py
@@ -5,7 +5,7 @@ from .forms import AddPlanetForm
 
 def index(request):
 
-    #fetch all existing Planet objects from database
+    #fetch all existing Planet objects from database to display
     cur_list = Planet.objects.all()
 
     # if user has entered form data
@@ -13,7 +13,14 @@ def index(request):
         form = AddPlanetForm(request.POST)
         if form.is_valid():
             #clean data, error-check and add to db if needed
-            return HttpResponseRedirect('homepage/')
+            data = form.cleaned_data
+            new_planet = Planet(ordinality = data['ordinality'],
+                                name = data['name'],
+                                size = data['size'],
+                                distance = data['distance'],
+                                description = data['description'])
+            new_planet.save()
+            return HttpResponseRedirect('index')
 
     #if presenting page (GET)
     else:
