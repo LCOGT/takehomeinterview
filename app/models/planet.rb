2a0a50c8f05aac3567923c2571fc87d00d68d1ac
Michael Levin <michaelb.levin@gmail.com>
2018-05-20 15:43:55 -0700

    mbl - add model validations

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,31 @@
+# == Schema Information
+#
+# Table name: planets
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  size        :float
+#  distance    :float
+#  ordinality  :integer          not null
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Planet < ApplicationRecord
+  before_save { name.downcase! }
+  validates :name,
+            presence: true,
+            uniqueness: true
+  validates :size,
+            numericality: true,
+            :unless => Proc.new {|e| e.size.blank?}
+  validates :distance,
+            numericality: true,
+            :unless => Proc.new {|e| e.distance.blank?}
+  validates :ordinality,
+            numericality: { only_integer: true},
+            presence: true,
+            uniqueness: true,
+            :unless => Proc.new {|e| e.distance.blank?}
 end
